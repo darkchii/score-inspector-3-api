15b6d91899e0d77ff191a578d17836c287677126
Mark <engineermarkpro@gmail.com>
2025-11-24 00:56:41 +0100

    db stuff added

---
+++ helpers/db.js
@@ -0,0 +1,78 @@
+const { default: Sequelize } = require('@sequelize/core');
+const { MariaDbDialect } = require('@sequelize/mariadb');
+const { PostgresDialect } = require('@sequelize/postgres');
+const AltUserLiveModel = require('../models/AltUserLive');
+require('dotenv').config();
+
+let databases = {
+    inspector: new Sequelize(
+        {
+            dialect: MariaDbDialect,
+            database: process.env.MYSQL_DB,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASS,
+            host: process.env.MYSQL_HOST,
+            timezone: 'Europe/Amsterdam',
+            logging: false,
+            retry: {
+                max: 10
+            }
+        }
+    ),
+    inspector_teams: new Sequelize(
+        {
+            dialect: MariaDbDialect,
+            database: process.env.MYSQL_DB_TEAM,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASS,
+            host: process.env.MYSQL_HOST,
+            timezone: 'Europe/Amsterdam',
+            logging: false,
+            retry: {
+                max: 10
+            }
+        }
+    ),
+    osuAlt: new Sequelize(
+        {
+            dialect: PostgresDialect,
+            database: process.env.ALT_DB_DATABASE,
+            user: process.env.ALT_DB_USER,
+            password: process.env.ALT_DB_PASSWORD,
+            host: process.env.ALT_DB_HOST,
+            port: process.env.ALT_DB_PORT,
+            logging: false,
+            retry: {
+                max: 10
+            }
+        }
+    )
+};
+module.exports.Databases = databases;
+
+async function CheckConnection(database, timeout = 10000) {
+    //just race between database.authenticate and a timeout
+    let success = false;
+
+    await Promise.race([
+        database.authenticate().then(() => {
+            success = true;
+        }),
+        new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject(new Error('Connection timeout'));
+            }, timeout);
+        })
+    ]);
+
+    if (!success) {
+        throw new Error('Connection failed');
+    }
+
+    return success;
+}
+
+const AltUserLive = AltUserLiveModel(databases.osuAlt);
+
+module.exports.CheckConnection = CheckConnection;
+module.exports.AltUserLive = AltUserLive;
--- routes/routeUser.js
+++ routes/routeUser.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { AuthorizeCodeGrant, GetOwnData, Search } = require('../helpers/osuApiHelper');
+const { Search } = require('../helpers/osuApiHelper');
+const { AltUserLive, CheckConnection, Databases } = require('../helpers/db');
 const router = express.Router();
 
 router.get('/search', async (req, res) => {
@@ -19,6 +20,38 @@ router.get('/search', async (req, res) => {
         console.error('Error during user search:', error);
         return res.status(500).json({ error: 'Internal server error' });
     }
+});
+
+router.get('/:userId/profile', async (req, res) => {
+    const { userId } = req.params;
+    if (!userId) {
+        return res.status(400).json({ error: 'User ID parameter is required' });
+    }
+
+    //validate that userId is a number
+    if (isNaN(userId)) {
+        return res.status(400).json({ error: 'User ID must be a number' });
+    }
+
+    try {
+        //check connection
+        const user = await AltUserLive.findOne({ where: { user_id: userId } });
+        if (user) {
+            return res.status(200).json(user);
+        } else {
+            return res.status(404).json({ error: 'User not found' });
+        }
+    }catch(error){
+        console.error('Error during user profile retrieval:', error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    return res.status(501).json({ error: 'Not implemented yet' });
+})
+
+router.get('/:userId/scores', async (req, res) => {
+    // TODO: Implement user scores retrieval
+    return res.status(501).json({ error: 'Not implemented yet' });
 });
 
 module.exports = router;
