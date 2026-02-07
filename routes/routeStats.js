f08774793fb03af451e369e93aa15f32599022e6
amayakase <engineermarkpro@gmail.com>
2026-02-07 20:21:35 +0100

    score submissions endpoint

--- routes/routeStats.js
+++ routes/routeStats.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { InspectorStat } = require('../helpers/db');
 const { getFullUsers } = require('../helpers/userHelper');
+const { OSU_SLUGS } = require('../helpers/osuHelper');
 const router = express.Router();
 
 router.get('/top-day', async (req, res) => {
@@ -80,6 +81,28 @@ router.get('/global-stats', async (req, res) => {
         });
         res.json(result);
     }catch (err) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+router.get('/score-submissions/:ruleset', async (req, res) => {
+    const { ruleset } = req.params;
+    try {
+        if(!Object.keys(OSU_SLUGS).includes(ruleset)){
+            return res.status(400).json({ error: 'Invalid ruleset' });
+        }
+
+        const ruleset_id = OSU_SLUGS[ruleset];
+        const data = await InspectorStat.findOne({ where: { metric: `score_data_counts_ruleset_${ruleset_id}` } });
+        if(!data || !data.data){
+            return res.status(404).json({ error: 'No data found' });
+        }
+
+        res.json({
+            data: JSON.parse(data.data),
+            last_updated: data.last_updated
+        });
+    }catch(err){
         res.status(500).json({ error: 'Internal server error' });
     }
 });
